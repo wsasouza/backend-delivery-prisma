6259fdd51b1056990c8c7b58d35ed75cc45784f5
wsasouza <wsasouza@hotmail.com>
2022-01-24 00:30:29 -0300

    Feat: Create Client

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import { routes } from './routes';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 app.get('/', (request, response) => {
   return response.json({
-    message: 'Hello app'
+    message: 'Delivery app is working'
   });
 });
 
