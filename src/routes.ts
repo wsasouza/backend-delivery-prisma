447c64116bc281386cde7b1f76020ddf219093e8
wsasouza <wsasouza@hotmail.com>
2022-01-25 14:37:13 -0300

    Feat: Create Delivery

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticationDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
@@ -12,10 +13,15 @@ const authenticateClientController = new AuthenticateClientController();
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 
+const createDeliveryController = new CreateDeliveryController();
+
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 routes.post('/client/authenticate', authenticateClientController.handle);
 
 routes.post('/client', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
+
+routes.post('/delivery', createDeliveryController.handle);
+
 
 export { routes };
