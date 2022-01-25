fce84adc65183f0158488368b50f8845bfc2e421
wsasouza <wsasouza@hotmail.com>
2022-01-25 12:59:16 -0300

    Feat: Create and Auth Deliveryman

---
+++ src/modules/account/authenticateDeliveryman/AuthenticateDeliverymanUseCase.ts
@@ -0,0 +1,37 @@
+import { prisma } from '../../../database/prismaClient';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+interface IAuthenticateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateDeliverymanUseCase {
+
+  async execute({username, password}: IAuthenticateDeliveryman) {
+
+    const deliveryman = await prisma.deliveryman.findFirst({
+      where: {
+        username
+      }
+    });
+
+    if(!deliveryman) {
+      throw new Error('Username or password invalid!');
+    }
+
+    const passwordMatch = await compare(password, deliveryman.password);
+
+    if(!passwordMatch) {
+      throw new Error('Username or password invalid!');
+    }  
+
+    const token = sign({ username }, '410dd201295871f313bac4047c53678d', {
+      subject: deliveryman.id,
+      expiresIn: '1d'
+    });
+
+    return token;    
+  }
+}
---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,33 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+
+  async execute({ username, password }: ICreateDeliveryman) {
+    const deliverymanExist = await prisma.deliveryman.findFirst({
+      where: {
+        username
+      }
+    });
+    
+    if(deliverymanExist) {
+      throw new Error('Deliveryman already exists');
+    }
+    
+    const hashPassword = await hash(password, 10);
+
+    const client = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    });
+
+    return client;
+  }
+}
